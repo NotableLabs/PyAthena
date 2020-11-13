eb5e3436514eefe222220d0761ee60ba9afd0fed
Steven <steven@notablelabs.com>
2020-11-12 16:59:22 -0800

    chore: notable version

--- pyathena/__init__.py
+++ pyathena/__init__.py
@@ -4,7 +4,7 @@ from typing import FrozenSet, Type
 
 from pyathena.error import *  # noqa
 
-__version__: str = "2.0.0"
+__version__: str = "2.0.0.notable"
 
 # Globals https://www.python.org/dev/peps/pep-0249/#globals
 apilevel: str = "2.0"
